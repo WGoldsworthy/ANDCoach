c45ae071deb568fbd12543e7a5635e66e9037c8b
Billvamangal Praviendran <billvamangalpraviendran@billvamngalsmbp.home>
2019-02-23 23:36:45 +0000

    SASS configuration done and necessary files added to the project

--- andcoach/src/App.js
+++ andcoach/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.sass';
 
 class App extends Component {
   render() {
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Accelerating your digital aspirations</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
