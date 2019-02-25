39177f44cfbd018cd45a3b8d2ab48e79524a1ca6
Billvamangal Praviendran <billvamangalpraviendran@Billvamangals-MacBook-Pro.local>
2019-02-25 09:24:37 +0000

    Initial google login setup added to the project

--- andcoach/src/components/Login/Login.test.js
+++ andcoach/src/components/Login/Login.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Login from './Login';
+import GoogleLogin from '../../App';
 
-describe('Login', () => {
+describe('GoogleLogin', () => {
   it('should render login button', () => {
-    const wrapper = shallow(<Login />)
+    const wrapper = shallow(<GoogleLogin />)
     expect(wrapper.length).toBe(1);
   });
 });
