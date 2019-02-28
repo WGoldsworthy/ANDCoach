7f1239dc095689099cfff1685f451d7bb1250463
George Rokkos <georgerokkos@Georges-MacBook-Pro.local>
2019-02-28 12:19:41 +0000

    evidence update completed

--- andcoach/src/objectivesController.js
+++ andcoach/src/objectivesController.js
@@ -58,10 +58,20 @@ class objectivesController {
 
     }
 
-    // Update objectives title and description
+    // Update objectives status
     static updateObjectiveStatus(req, res) {
         const id = req.params.id;
         Objective.findOneAndUpdate({_id: id}, {$set: {"status": req.body.status}}, function(err) {
+            if (err) return res.send(500, {error: err});
+            return res.json({success: true, id: id});
+        });
+
+    }
+
+    // Update evidence
+    static updateEvidence(req, res) {
+        const id = req.params.id;
+        Objective.findOneAndUpdate({_id: id}, {$set: {"evidence": req.body.evidence}}, function(err) {
             if (err) return res.send(500, {error: err});
             return res.json({success: true, id: id});
         });
